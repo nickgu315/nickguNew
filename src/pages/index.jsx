0b986c27703379e5d62c28964fc4b1909e5c7aeb
nickgu315 <gulikh@gmail.com>
2022-11-09 13:15:54 -0800

    landing changed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,22 +5,20 @@ import Instructions from '@/components/dom/Instructions'
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
 // https://github.com/pmndrs/react-three-next/issues/49
-const Logo = dynamic(() => import('@/components/canvas/Logo'), { ssr: false })
+const CabinSample = dynamic(() => import('@/components/canvas/CabinSample'), { ssr: false })
 
 // Dom components go here
 export default function Page(props) {
   return (
-    <Instructions>
-      This is a minimal starter for Nextjs + React-three-fiber and Threejs. Click on the{' '}
-      <span className='text-cyan-200'>atoms nucleus</span> to navigate to the{' '}
-      <span className='text-green-200'>/blob</span> page. OrbitControls are enabled by default.
-    </Instructions>
+    <div>
+      By Nick
+    </div>
   )
 }
 
 // Canvas components go here
 // It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => <Logo scale={0.5} route='/blob' position-y={-1} />
+Page.canvas = (props) => <CabinSample scale={0.5} route='/' position-y={-1} />
 
 export async function getStaticProps() {
   return { props: { title: 'Index' } }
